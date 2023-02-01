6689266437babbfe4c47350113242ec169c21dc8
Kasun <alankasun@gmail.com>
2023-02-01 20:51:35 +0530

    chore: next canary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 import Render from "./render";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Render />
     </main>
   );
